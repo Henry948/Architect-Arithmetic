d6564f38fcaeada97f599367e1509aa29e7411d4
dahdi <62443126+dahdi@users.noreply.github.com>
2021-01-26 12:30:26 +0000

    created base methods for each shape of the floorplan

--- Program.cs
+++ Program.cs
@@ -8,5 +8,15 @@ namespace ArchitectArithmetic
     {
 
     }
+
+    public static double Rectangles(double length, double width) {
+        double area = length * width;
+    }
+    public static double Circles(double pi = Math.PI(), double radius = Math.Pow()){
+        double area = pi * radius;
+    }
+    public static double Triangles(double bottom, double height) {
+      area = 0.5 * bottom * height;
+    }
   }
 }
