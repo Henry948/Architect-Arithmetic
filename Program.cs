abfe9c3b40f90ad3550eb470da8e384ae805e505
dahdi <62443126+dahdi@users.noreply.github.com>
2021-01-27 16:23:35 +0000

    Created the overal calculations
    added comments for each section to split the code up.
    
    Calculated the area of each shape within the floorplan.
    
    created if statment for a full and semi-circle.
    
    calculated the overal price and printed it to console.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,44 @@ namespace ArchitectArithmetic
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine(Rectangles(4, 5));
-      Console.WriteLine(Circles(4));
-      Console.WriteLine(Triangles(10, 9));
+      // to store and calculate the rectangle area of floor plan
+
+      Console.Write("please enter the rectangle length: ");
+      double rectangleLength = Console.ReadLine();
+      Console.Write("Please enter the height of the rectangle: ");
+      double rectangleHeight = Console.ReadLine();
+      double rectangleArea = Rectangles(rectangleLength, rectangleHeight);
+
+      // to store and calculate the circle area in the floor plan
+
+      Console.Write("please enter the radius of the circle: ")
+      double circleRadius = Console.ReadLine();
+      Console.Write("Is this a semi-circle> YES / NO");
+      string answer = Console.ReadLine();
+      answer.ToUpper();
+      double circleArea;
+      if (answer = "YES") {
+        circleArea = Circles(CircleRadius) / 2;
+      } else {
+        circleArea = Circles(CircleRadius);
+      }
+      // to store and calculate the triangle area on the floor plan
+
+      Console.Write("Enter the bottom value of the triangle: ");
+      double triangleBottom = Console.ReadLine();
+      Console.Write("Enter the height value of the triangle: ");
+      double triangleHeight = Console.ReadLine();
+      double trianglearea = Triangles(triangleBottom, traingleHeight);
+
+      // total area is calculated below
+
+      double floorPlanArea = rectangleArea + circleArea + triangleArea;
+      Console.WriteLine($"The area of the complete floorplan is {floorPlanArea}m²");
+
+      // The code below calculates the cost in gbp per square meter
+      int pricePerSqareMeter = 50;
+      bool floorPlanPrice = pricePerSquareMeter * floorPlanArea;
+      Console.WriteLine($"The total price for the flooring will cost {floorPlanPrice} to cover {floorPlanArea}m²");
     }
 
     static double Rectangles(double length, double width) {
