851fb9cd019d1db835c3b8e5514b3d6a62f02e28
dahdi <62443126+dahdi@users.noreply.github.com>
2021-03-23 23:35:56 +0000

    Added user input for how many of each shape can be selected
    Used a series of Arrays and while loops to allow the user to input their desired amounts of each shape and then fill in the information accordingly. This simplifies things and stops the user from having to input values of 0 for each value used in calculations if the shape is not included. Application now caters to all users rather than one specific business. Will be creating a front end for this application in a new project. will be either windows form application or web application.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-
+using System.Linq;
 namespace ArchitectArithmetic
 {
   class Program
@@ -7,40 +7,81 @@ namespace ArchitectArithmetic
     public static void Main(string[] args)
     {
 
+      int numberOfRectangles;
+      int numberOfCircles;
+      int numberOfTriangles;
+
+      double[] rectangleAreaArray = new double[] {0};
+      double[] circleAreaArray = new double[] {0};
+      double[] triangleAreaArray = new double[] {0};
+
      // to store and calculate the rectangle area of floor plan
-      Console.Write("please enter the rectangle length: ");
-      double rectangleLength = Convert.ToDouble(Console.ReadLine());
-      Console.Write("Please enter the height of the rectangle: ");
-      double rectangleHeight =  Convert.ToDouble(Console.ReadLine());
-      Convert.ToDouble(rectangleHeight);
-      double rectangleArea = Rectangles(rectangleLength, rectangleHeight);
+     Console.Write("How many rectangles are in the floor plan? ");
+     string numberOfRectanglesAsString = Console.ReadLine();
+     numberOfRectangles =  Convert.ToInt32(numberOfRectanglesAsString);
+
+     while (numberOfRectangles > 0) {
+        Console.Write("please enter the rectangle length: ");
+        double rectangleLength = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Please enter the height of the rectangle: ");
+        double rectangleHeight =  Convert.ToDouble(Console.ReadLine());
+        Convert.ToDouble(rectangleHeight);
+        double rectangleArea = Rectangles(rectangleLength, rectangleHeight);
+        rectangleAreaArray = rectangleAreaArray.Concat(new double[] {rectangleArea}).ToArray();
+        numberOfRectangles--;
+      }
+
+      double totalRectangleArea = rectangleAreaArray.Sum();
 
       // to store and calculate the circle area in the floor plan
 
-      Console.Write("please enter the radius of the circle: ");
-      double circleRadius = Convert.ToDouble(Console.ReadLine());
-      Console.Write("Is this a semi-circle? YES / NO: ");
-      string answer = Console.ReadLine();
-      answer.ToUpper();
-      double circleArea;
-      if (answer == "YES") {
-        circleArea = Circles(circleRadius) / 2;
-      } else {
-        circleArea = Circles(circleRadius);
+      Console.Write("How many circles are in the floor plan? ");
+      string numberOfCirclesAsString = Console.ReadLine();
+      numberOfCircles =  Convert.ToInt32(numberOfCirclesAsString);
+
+      while (numberOfCircles > 0) {
+        Console.Write("please enter the radius of the circle: ");
+        double circleRadius = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Is this a semi-circle? YES / NO: ");
+        string answer = Console.ReadLine();
+        answer.ToUpper();
+        double circleArea;
+        if (answer == "YES") {
+          circleArea = Circles(circleRadius) / 2;
+        } else {
+          circleArea = Circles(circleRadius);
+        }
+        circleAreaArray = circleAreaArray.Concat(new double[] {circleArea}).ToArray();
+
+        numberOfCircles--;
       }
+
+      double totalCircleArea = circleAreaArray.Sum();
+
       // to store and calculate the triangle area on the floor plan
 
-      Console.Write("Enter the bottom value of the triangle: ");
-      string triangleBottom = Console.ReadLine();
-      double triBottomAsDouble = Convert.ToDouble(triangleBottom);
-      Console.Write("Enter the height value of the triangle: ");
-      string triangleHeight = Console.ReadLine();
-      double triHeightAsDouble = Convert.ToDouble(triangleHeight);
-      double triangleArea = Triangles(triBottomAsDouble, triHeightAsDouble);
+      Console.Write("How many triangles are in the floor plan? ");
+      string numberOfTrianglesAsString = Console.ReadLine();
+      numberOfTriangles =  Convert.ToInt32(numberOfTrianglesAsString);
+
+      while (numberOfTriangles > 0) {
+        Console.Write("Enter the bottom value of the triangle: ");
+        string triangleBottom = Console.ReadLine();
+        double triBottomAsDouble = Convert.ToDouble(triangleBottom);
+        Console.Write("Enter the height value of the triangle: ");
+        string triangleHeight = Console.ReadLine();
+        double triHeightAsDouble = Convert.ToDouble(triangleHeight);
+        double triangleArea = Triangles(triBottomAsDouble, triHeightAsDouble);
+        triangleAreaArray = triangleAreaArray.Concat(new double[] {triangleArea}).ToArray();
+
+        numberOfTriangles--;
+      }
+
+      double totalTriangleArea = triangleAreaArray.Sum();
 
       // total area is calculated below
 
-      double floorPlanArea = rectangleArea + circleArea + triangleArea;
+      double floorPlanArea = totalRectangleArea + totalCircleArea + totalTriangleArea;
       Console.WriteLine($"The area of the complete floorplan is {floorPlanArea}m²");
 
       // The code below calculates the cost in gbp per square meter
